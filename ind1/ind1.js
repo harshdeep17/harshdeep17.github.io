2e48f7542fa00a3397febdcca2558b775f82ff91
harshdeep17 <76818788+harshdeep17@users.noreply.github.com>
2021-01-14 10:33:33 +0530

    Update ind1.js

--- ind1/ind1.js
+++ ind1/ind1.js
@@ -10,12 +10,13 @@ function ageInDays(){
     var birthyear=prompt('what year are you born...good friend?');
     var ageInDayss=(2020-birthyear)*365;
     var h1=document.createElement('h1');
+    h1.setAttribute('id','ageInDays');
     var textAnswer=document.createTextNode('you are '+ageInDayss+' days old');
     h1.appendChild(textAnswer);
     document.getElementById('flex-box-result').appendChild(h1);
 } 
 function reset(){
-    document.getElementById('flex-box-result').remove();
+    document.getElementById('ageInDays').remove();
 }
 function generateCat(){
     var image=document.createElement('img');
